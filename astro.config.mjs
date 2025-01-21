c22a6c9e0157f19a4f08d3edc1255e091bac17b8
Satakun Utama <linesofcodes@dailitation.xyz>
2025-01-21 20:07:44 +0700

    Added TH version

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: "https://lines-of-codes.github.io",
+    i18n: {
+        locales: ["en", "th"],
+        defaultLocale: "en"
+    },
     integrations: [tailwind()]
 });
